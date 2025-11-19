893360d64451e20cf5f52e32b1122165ded944c2
Jules Maulard <jules.maulard@enseirb-matmeca.fr>
2025-11-19 15:15:19 +0100

    feat: add button redirection

---
+++ app/src/main/java/com/example/ckoa/DailyGameActivity.java
@@ -0,0 +1,5 @@
+package com.example.ckoa;
+
+public class DailyGameActivity {
+
+}
--- app/src/main/java/com/example/ckoa/MainActivity.java
+++ app/src/main/java/com/example/ckoa/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.ckoa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDaily, btnHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnDaily = findViewById(R.id.btnDaily);
+        btnHistory = findViewById(R.id.btnHistory);
+
+        btnDaily.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DailyGameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO change DailyGameActivity to HistoryActivity
+                Intent intent = new Intent(MainActivity.this, DailyGameActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
