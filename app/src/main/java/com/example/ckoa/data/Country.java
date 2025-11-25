97e27b68afab0cea4162f7252a6d11abe2722390
Laora <laora.aimi@gmail.com>
2025-11-25 16:03:55 +0100

    feat: Add structure of Country and CountryGuess and using the API

--- app/src/main/java/com/example/ckoa/data/Country.java
+++ app/src/main/java/com/example/ckoa/data/Country.java
@@ -1,4 +1,61 @@
 package com.example.ckoa.data;
 
 public class Country {
+    private final String iso3;
+    private final String englishName;
+    private final String frenchName;
+    private final String flag;
+    private final String[] borders;
+    private final String currency;
+    private final String capital;
+    private final String[] languages;
+    private final Integer population;
+
+    public Country(String iso3, String englishName, String frenchName, String flag, String[] borders, String currency, String capital, String[] languages, Integer population) {
+        this.iso3 = iso3;
+        this.englishName = englishName;
+        this.frenchName = frenchName;
+        this.flag = flag;
+        this.borders = borders;
+        this.currency = currency;
+        this.capital = capital;
+        this.languages = languages;
+        this.population = population;
+    }
+
+    public String getIso3() {
+        return iso3;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public String getFrenchName() {
+        return frenchName;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public String[] getBorders() {
+        return borders;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
 }
--- app/src/main/java/com/example/ckoa/data/CountryGuess.java
+++ app/src/main/java/com/example/ckoa/data/CountryGuess.java
@@ -1,4 +1,25 @@
 package com.example.ckoa.data;
 
 public class CountryGuess {
+    private final String iso3;
+    private final String englishName;
+    private final String frenchName;
+
+    public CountryGuess(String iso3, String englishName, String frenchName) {
+        this.iso3 = iso3;
+        this.englishName = englishName;
+        this.frenchName = frenchName;
+    }
+
+    public String getIso3() {
+        return iso3;
+    }
+
+    public String getFrenchName() {
+        return frenchName;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
 }
