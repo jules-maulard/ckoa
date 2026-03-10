4874f489de863ba5a765330a136f5cad677b90ad
Jules Maulard <jules.maulard@enseirb-matmeca.fr>
2026-03-10 23:36:04 +0100

    build(deps): add gson dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
